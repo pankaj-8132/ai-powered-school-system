0f750a2c2f973285393247a8bf262d37dd25b181
pankaj-8132 <pankaj81328@gmail.com>
2026-04-01 06:22:58 +0530

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  withCredentials: true, // ✅ This is the key
+  baseURL: "https://ai-powered-school-system-10-7.onrender.com",
+  withCredentials: true,
 });
